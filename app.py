1a2350aedde7aabdb53fd6451fef5b693fb78977
xavier <x@xavier.dev>
2024-03-22 20:16:00 +0200

    Added registration, login pages.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from os import getenv
@@ -18,10 +18,44 @@ def index():
 def new():
     return render_template("new.html")
 
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/login_user",methods=["POST"])
+def login_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.html", name=request.form["name"])
+
 @app.route("/send", methods=["POST"])
 def send():
     content = request.form["content"]
     sql = "INSERT INTO messages (content) VALUES (:content)"
-    db.session.execute(sql, {"content":content})
+    db.session.execute(text(sql), {"content":content})
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/create_user", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password = request.form["password"]
+    sql = "INSERT INTO users (username, password) VALUES (:username, :password) RETURNING id"
+    result = db.session.execute(text(sql), {"username":username, "password":password})
+    user_id = result.fetchone()[0]
     db.session.commit()
     return redirect("/")
