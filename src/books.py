deb4c2d4130b5a179b6130178f4d39b2824e52c2
xavier <x@xavier.dev>
2024-03-30 15:55:04 +0100

    Cleaned code

--- src/books.py
+++ src/books.py
@@ -48,33 +48,3 @@ def fetch_book_by_id(id):
     result = db.session.execute(text(sql), {"id":id})
     return result.fetchone()    
 
-def logout():
-    del session["username"]
-    del session["user_id"]
-    return True 
-
-def register(username, password):
-    hash_value = generate_password_hash(password)
-    print("GENERATED HASH VALUE")
-    try:
-        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
-        db.session.execute(text(sql), {"username":username, "password":hash_value})
-        db.session.commit()
-    except:
-        return False
-    return login(username, password)
-
-'''
-def register():
-    username = request.form["username"]
-    password = request.form["password"]
-    hash_value = generate_password_hash(password)
-    sql = "INSERT INTO users (username, password) VALUES (:username, :password) RETURNING id"
-    result = db.session.execute(text(sql), {"username":username, "password":hash_value})
-    user_id = result.fetchone()[0]
-    db.session.commit()
-    return redirect("/")
-'''
-def user_id():
-    print("session id "+session["username"])
-    return session.get("user_id",0)
