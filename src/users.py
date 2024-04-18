347a8b2f8e90ba64089f78a0515a427e2a318794
xavier <x@xavier.dev>
2024-04-19 00:37:28 +0200

    Added username_free function:

--- src/users.py
+++ src/users.py
@@ -2,6 +2,7 @@ from db import db
 from flask import session, redirect 
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy import text
+import secrets
 
 def login(username, password):
     sql = "SELECT id, password FROM users WHERE username=:username"
@@ -15,6 +16,7 @@ def login(username, password):
             #correct username and password
             session["username"] = username
             session["user_id"] = user.id
+            session["csrf_token"] = secrets.token_hex(16)
             return True 
         else:
             #invalid password
@@ -25,9 +27,23 @@ def logout():
     del session["user_id"]
     return True 
 
+def username_free(username):
+    print("Checking if username_free")
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(text(sql), {"username":username})
+    user = result.fetchone()    
+    if user:
+        return False 
+    return True
+
+
 def register(username, password):
+    print("Checking if username_free in register")
+    if not username_free(username):
+        return False
     hash_value = generate_password_hash(password)
     print("GENERATED HASH VALUE")
+
     try:
         sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
         db.session.execute(text(sql), {"username":username, "password":hash_value})
@@ -38,3 +54,6 @@ def register(username, password):
 
 def user_id():
     return session.get("user_id",0)
+
+def user_session():
+    return session.get("csrf_token",0)
