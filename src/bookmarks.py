ffe6287146a0f2b5aef03cba7f9b87ec4662ce44
xavier <x@xavier.dev>
2024-04-21 21:26:01 +0200

    Added save bookmark, delete bookmark by id and delete all bookmarks of a pair:

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,6 +1,37 @@
 from db import db
 from sqlalchemy import text
 
+def save_bookmark(pair_id,counter1, counter2):
+    try:
+        db.session.commit()
+        sql = "INSERT INTO bookmarks (pair_id, counter1, counter2) VALUES (:pair_id,:counter1,:counter2);"
+        db.session.execute(text(sql), {"pair_id":pair_id,"counter1":counter1,"counter2":counter2 })
+        db.session.commit()
+    except Exception as e:
+        print(f"Error for inserting new bookmark into bookmarks: {e}")
+        return False 
+    return True
+
+def delete_bookmark_by_id(bookmark_id):
+    try:
+        sql = "DELETE FROM bookmarks WHERE id=:bookmark_id;"
+        db.session.execute(text(sql), {"bookmark_id":bookmark_id })
+        db.session.commit()
+    except Exception as e:
+        print(f"Error for deleting bookmark of id {bookmark_id}: {e}")
+        return False 
+    return True
+
+def delete_all_bookmarks_of_pair_by_pair_id(pair_id):
+    try:
+        sql = "DELETE FROM bookmarks WHERE pair_id=:pair_id;"
+        db.session.execute(text(sql), {"pair_id":pair_id })
+        db.session.commit()
+    except Exception as e:
+        print(f"Error for deleting all bookmark of pair_id {pair_id}: {e}")
+        return False 
+    return True
+
 def fetch_bookmarks_by_pair_id(pair_id):
     try:
         print(pair_id, type(pair_id))
