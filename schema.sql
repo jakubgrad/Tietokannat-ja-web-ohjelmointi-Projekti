4add5b7c6df8264ccce4cfc82da7d91fd813daa8
xavier <x@xavier.dev>
2024-05-05 16:48:28 +0200

    Removed old table:

--- schema.sql
+++ schema.sql
@@ -2,9 +2,7 @@ DROP TABLE IF EXISTS bookmarks;
 DROP TABLE IF EXISTS pairs;
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS messages;
 
-CREATE TABLE messages (id SERIAL PRIMARY KEY, content TEXT);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
 CREATE TABLE books (id SERIAL PRIMARY KEY, title TEXT, user_id INTEGER REFERENCES users, filename TEXT, language TEXT, author TEXT, isbn TEXT, json JSONB);
 CREATE TABLE pairs (   id SERIAL PRIMARY KEY,  user_id INTEGER REFERENCES users, name TEXT,  created_at TIMESTAMP, book1_id INTEGER REFERENCES books,  book2_id INTEGER REFERENCES books );
